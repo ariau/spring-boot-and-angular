45203d3cfa185f8bf018889dc1ff3764d0aa969b
ariau <alexisdeclairebois@gmail.com>
2020-08-27 22:36:45 -0400

    clean

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 //POJO : Plain Old Java Object. model qui a les infos de purchase du vélo
 //Hibernate implemente automatiquement des méthdde qu'on ne veut pas utiliser car on préfère utiliser Jackson (JSON)
