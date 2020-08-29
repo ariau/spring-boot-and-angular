844641a687eb89765e989efcdd617e6f42ad3afe
ariau <alexisdeclairebois@gmail.com>
2020-08-29 18:18:00 -0400

    route for view registration

--- bike-ui/src/app/app-routing.module.ts
+++ bike-ui/src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 
 const routes: Routes = [
   {
-    path:"home",
+    path:"",
     component:HomeComponent
-  }
-  ,{
+  },
+  {
+    path:"admin/view/:id",
+    component: ViewRegistrationComponent
+  },
+  {
     path:"admin",
     component:AdminComponent
   }
