1b478cd0c8225a7d940df13059988e36732f89e8
ariau <alexisdeclairebois@gmail.com>
2020-10-02 22:05:15 -0400

    pom xml update

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -3,39 +3,41 @@ package com.globomatics.bike.controllers;
 import com.globomatics.bike.models.Bike;
 import com.globomatics.bike.repositories.BikeRepository;
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 
-    @Autowired
-    private BikeRepository bikeRepository;
+  @Autowired private BikeRepository bikeRepository;
 
-    @GetMapping
-    public List<Bike> bikes() {
-        var it = bikeRepository.findAll();
+  @GetMapping
+  public List<Bike> bikes() {
+    List it = bikeRepository.findAll();
 
-        List bikes = new ArrayList<Bike>();
-        it.forEach(e -> bikes.add(e));
+    List bikes = new ArrayList<Bike>();
+    it.forEach(e -> bikes.add(e));
 
-        return bikes;
-    }
+    return bikes;
+  }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public void create(@RequestBody Bike bike) {
-        bikeRepository.save(bike);
-    }
+  @PostMapping
+  @ResponseStatus(HttpStatus.OK)
+  public void create(@RequestBody Bike bike) {
+    bikeRepository.save(bike);
+  }
 
-    @GetMapping("/{id}")
-    public Bike get(@PathVariable("id") Long id) {
-        return bikeRepository.getOne(id);
-    }
-
+  @GetMapping("/{id}")
+  public Bike get(@PathVariable("id") Long id) {
+    return bikeRepository.getOne(id);
+  }
 }
