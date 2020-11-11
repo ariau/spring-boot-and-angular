6cbec5a080c5a125d45be9d14f90994f300dbf5e
ariau <alexisdeclairebois@gmail.com>
2020-11-10 21:01:39 -0500

    enabling cors

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/bikes")
+@CrossOrigin("https://sprint-boot-angular-ui.herokuapp.com/")
 public class BikesController {
 
   @Autowired
